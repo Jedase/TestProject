d90933e0ec4dfe690cf9b199ea18b56c9f98f249
Jedase <102157393+Jedase@users.noreply.github.com>
2022-03-23 11:34:25 +0700

    Add files via upload

--- cypress/integration/NewProject/Project.js
+++ cypress/integration/NewProject/Project.js
@@ -7,29 +7,28 @@ describe('My First Test', () => {
     })
   
 
-  it('Создание чата', () => {
-    const newAdress = '0x228c3c67491d0e78f5b0a40caf83339806f25e4f'
-    cy.get('.circle-button__icon').click()
-    cy.get('[data-selector=ACTION-USER-ADDRESS]').type(`${newAdress}{enter}`)
-    cy.get('.base-modal__buttons > .base-button_white').click()
-    cy.contains('Close').click()
-    cy.wait(5000)
-  })
-
-
-  it('Отправка сообщения в созданный чат', () => {
-    const newText = "Привет, это проверка чата!"
-    cy.get('.contacts__bottom > :nth-child(1)').click()
-    cy.get('#id-Message').type(`${newText}{enter}`)
-    cy.get('.base-textarea > .circle-button > .circle-button__icon').click()
-  })
+    it('Создание чата', () => {
+      const newAdress = '0x228c3c67491d0e78f5b0a40caf83339806f25e4f'
+      cy.get('.circle-button__icon').click()
+      cy.get('[data-selector=ACTION-USER-ADDRESS]').type(`${newAdress}{enter}`)
+      cy.get('.base-modal__buttons > .base-button_white').click()
+      cy.contains('Close',{timeout: 6000}).click()
+    })
+  
+  
+    it('Отправка сообщения в созданный чат', () => {
+      const newText = "Привет, это проверка чата!"
+      cy.get('.contacts__bottom > :nth-child(1)').click()
+      cy.get('#id-Message').type(`${newText}{enter}`)
+      cy.get('.base-textarea > .circle-button > .circle-button__icon').click()
+    })
 
 
   it('Отправка оффера для созданного чата', () => {
     const newDecimal = '1'
     cy.get('[data-selector=ACTION-OPEN-OFFER-CREATION]').click()
     cy.get(':nth-child(3) > .base-dropdown > .base-dropdown__input').click()
-    cy.wait(6000)
+    cy.wait(5000)
     cy.get('.base-modal__tokens > :nth-child(1)').click()
     cy.get('[data-selector=ACTION-SENDING-AMOUNT]').type(`${newDecimal}{enter}`)
     cy.get('.fields > .base-dropdown > .base-dropdown__input').click()
@@ -47,3 +46,7 @@ describe('My First Test', () => {
     cy.get('.dropdown__disconnect').click()
   })
 })
+
+
+
+//{"animationDistanceThreshold": 7000}
